5b9075a518f06ca7a1cf43eaf82dc48607620551
solen <kassahunsolen@gmail.com>
2025-12-01 10:34:26 +0300

    Create arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,12 @@
+def perform_operation(num1, num2, operation):
+    """Perform basic arithmetic operations: add, subtract, multiply, or divide."""
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
